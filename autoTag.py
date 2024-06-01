ff30938cc2c2863f065ad210f924c56b43b89ad7
xvs32 <makaut1995.cs02@g2.nctu.edu.tw>
2024-06-01 09:56:15 +0800

    Change output from graph to name

--- autoTag.py
+++ autoTag.py
@@ -7,19 +7,22 @@ import matplotlib.pyplot as plt
 with open('msd-musicnn-1.json', 'r') as json_file:
     metadata = json.load(json_file)
 
-print(metadata.keys())
-
 audio_file = '/media/marco/share_area/Git/YTPipeline/AllMyself.wav'
 audio = MonoLoader(sampleRate=16000, filename=audio_file)()
 
 activations = TensorflowPredictMusiCNN(graphFilename='msd-musicnn-1.pb')(audio)
 
-ig, ax = plt.subplots(1, 1, figsize=(10, 10))
-ax.matshow(activations.T, aspect='auto')
+result = None
 
-ax.set_yticks(range(len(metadata['classes'])))
-ax.set_yticklabels(metadata['classes'])
-ax.set_xlabel('patch number')
-ax.xaxis.set_ticks_position('bottom')
-plt.title('Tag activations')
-plt.show()
+for slice in activations:
+    if result is None:
+        result = slice
+    else:
+        result += slice
+        
+#find the id of the highest activation 
+id = np.argmax(result)
+
+print(id)
+print(result)
+print(metadata["classes"][id])
